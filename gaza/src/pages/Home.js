e3dd62969c012b400f074c923395c0c1356d9431
bjennin4 <BJennin4@gmail.com>
2024-04-27 14:43:19 -0400

    Style changes

--- gaza/src/pages/Home.js
+++ gaza/src/pages/Home.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import '../App.css';
+
 const Home = () => {
-    return <h1>Home</h1>;
-};
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Card className='card-container'>
+                <Card.Body>
+                    <Card.Title>Home Info</Card.Title>
+                        <Card.Text>
+                            This is the home page info.
+                        </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
 
 export default Home;
