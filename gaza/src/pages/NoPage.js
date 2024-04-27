0da9b032d036339059e08d52aec69bfa35bdbaff
bjennin4 <BJennin4@gmail.com>
2024-04-27 16:24:36 -0400

    All pages style updated
    Update for consistency

--- gaza/src/pages/NoPage.js
+++ gaza/src/pages/NoPage.js
@@ -1,5 +1,22 @@
+import '../App.css';
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
 const NoPage = () => {
-    return <h1>404</h1>;
+        return (
+        <div className='main-container'>
+            <Card className='card-container'>
+                <Card.Body>
+                    <Card.Title className='card-title'>404 Page Not Found</Card.Title>
+                        <Card.Text className='card-text'>
+                            The page you are trying to access was not found. 
+                            <br></br>
+                            The navbar above can be used to access a valid page. 
+                        </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 };
 
 export default NoPage;
