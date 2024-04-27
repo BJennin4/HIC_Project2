a22f15b1b895eb79530dc54face29b95b49b05df
bjennin4 <BJennin4@gmail.com>
2024-04-27 15:46:52 -0400

    Style update

--- gaza/src/pages/Home.js
+++ gaza/src/pages/Home.js
@@ -1,12 +1,12 @@
+import '../App.css';
+import '../index.css';
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import '../App.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <Card className = 'card-container'>
+        <div className='home-container'>
+            <Card className='card-container'>
                 <Card.Body>
                     <Card.Title>Home Info</Card.Title>
                         <Card.Text>
