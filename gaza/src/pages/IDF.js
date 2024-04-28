46a805b30142098650e8d3727f02d8c25089f21c
Konnor Duncan <kvd5051@g.rit.edu>
2024-04-27 22:02:29 -0400

    unfinished but working CurrentState, map png for later

--- gaza/src/pages/IDF.js
+++ gaza/src/pages/IDF.js
@@ -9,11 +9,21 @@ const IDF = () => {
                 <Card.Body>
                     <Card.Title className='card-title'>Israel Defense Forces</Card.Title>
                         <Card.Text className='card-text'>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare suspendisse. Proin sagittis nisl rhoncus mattis rhoncus urna. Vel eros donec ac odio tempor orci. Pretium nibh ipsum consequat nisl vel pretium lectus quam. Interdum varius sit amet mattis vulputate enim. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Et malesuada fames ac turpis egestas sed tempus urna et. Id leo in vitae turpis massa sed elementum tempus egestas. Id porta nibh venenatis cras. Ornare massa eget egestas purus viverra accumsan in. Cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla.</p>
-
-                        <p>Nunc faucibus a pellentesque sit amet porttitor eget dolor. Tellus integer feugiat scelerisque varius. Nec ultrices dui sapien eget mi. Hac habitasse platea dictumst vestibulum rhoncus est. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Et egestas quis ipsum suspendisse ultrices. Vestibulum morbi blandit cursus risus at. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Nibh praesent tristique magna sit amet purus gravida quis blandit. Tempor nec feugiat nisl pretium. Id donec ultrices tincidunt arcu non sodales. In hac habitasse platea dictumst vestibulum rhoncus. Etiam dignissim diam quis enim. Facilisi nullam vehicula ipsum a arcu cursus vitae. Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Semper eget duis at tellus at. Consectetur libero id faucibus nisl tincidunt eget. Urna neque viverra justo nec ultrices dui sapien eget mi. Quisque egestas diam in arcu cursus.</p>
-
-                        <p>Pretium viverra suspendisse potenti nullam ac tortor vitae. Mi proin sed libero enim sed. Nisl vel pretium lectus quam id. Egestas diam in arcu cursus euismod quis viverra nibh cras. Nec ullamcorper sit amet risus nullam eget felis eget nunc. Pretium viverra suspendisse potenti nullam ac tortor vitae. Cursus turpis massa tincidunt dui ut ornare lectus sit amet. Gravida rutrum quisque non tellus orci ac. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Amet facilisis magna etiam tempor orci eu lobortis. Laoreet id donec ultrices tincidunt arcu non sodales neque. Sollicitudin tempor id eu nisl nunc mi ipsum. Id diam vel quam elementum pulvinar.</p>
+                        The IDF was established on May 26, 1948 following Israel's Declaration of Independence
+                        </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='card-container'>
+                <Card.Body>
+                    <Card.Title className='card-title'></Card.Title>
+                        <Card.Text className='card-text'>
+                        </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='card-container'>
+                <Card.Body>
+                    <Card.Title className='card-title'></Card.Title>
+                        <Card.Text className='card-text'>
                         </Card.Text>
                 </Card.Body>
             </Card>
