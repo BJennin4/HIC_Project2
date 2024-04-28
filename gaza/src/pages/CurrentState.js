caecc87fe18fd7bbc5a4af819a28e4adc240cb9c
Konnor Duncan <kvd5051@g.rit.edu>
2024-04-28 16:53:16 -0400

    reformatted CurrentState page. IDF page has images that are not implemented correctly

--- gaza/src/pages/CurrentState.js
+++ gaza/src/pages/CurrentState.js
@@ -8,55 +8,35 @@ import Col from 'react-bootstrap/Col';
 const CurrentState = () => {
     return (
         <div className='main-container'>
-            
-                        <Card className="card-container">
-                            <Card.Body>
-                            <Card.Title>Gaza</Card.Title>
-                                <Card.Text>
-                    Killed: at least 31,726 people, including more than:
-	                    More than 13,000 children
-	                    84,000 women<br></br>
-                    Injured: more than 73,792 people, including at least:
-	                    8,663 children
-	                    6,327 women
-                    Missing: more than 8,000
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card className="card-container">
-                            <Card.Body>
-                            <Card.Title>Occupied West Bank</Card.Title>
-                                <Card.Text>
-                    Killed: at least 435 people, including more than 116 children
-                    Injured: more than 4,650 people
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card className="card-container">
-                            <Card.Body>
-                            <Card.Title>Israel</Card.Title>
-                                <Card.Text>
-                    Killed: 1,139
-                    Injured: at least 8,730
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                <br></br>
-                <Container>
-                <Row>
-                    <br></br>
                     <Card className='card-container'>
                         <Card.Body>
                         <Card.Title className='card-title'>Current State</Card.Title>
                             <Card.Text className='card-text'>
-                        Over two million Palestinians live at risk of hunger, preventable diseases, and air strikes. 
-                        Nearly 70 percent of the homes in Gaza have been destroyed or severely damaged leaving 75 percent of the population displaced. The displaced population lives in shelters built from scavenged material. 
-                        At night, temperatures can reach lows of 40F, causing preventable health issues in children, pregnant women, and elderly.
+                                    <p> Over two million Palestinians live at risk of hunger, preventable diseases, and air strikes. 
+                                     Nearly 70 percent of the homes in Gaza have been destroyed or severely damaged leaving 75 percent of the population displaced. The displaced population lives in shelters built from scavenged material. 
+                                     At night, temperatures can reach lows of 40F, causing preventable health issues in children, pregnant women, and elderly.
+                                    </p>
+                                <h2>Civilian Casualities</h2>
+                                    <h3>Gaza</h3>
+                                    <p>Killed: at least 31,726 people, including more than:
+	                                 More than 13,000 children
+	                                 84,000 women<br></br>
+                                     Injured: more than 73,792 people, including at least: 8,663 children, 6,327 women<br></br>
+                                    Missing: more than 8,000
+                                    </p>
+                                    <h3>Occupied West Bank</h3>
+                                   <p> Killed: at least 435 people, including more than 116 children<br></br>
+                                    Injured: more than 4,650 people</p>
+                                    <h3>Israel</h3>
+                                     <p>Killed: 1,139<br></br>
+                                      Injured: at least 8,730</p>
                             </Card.Text>
                     </Card.Body>
                     </Card>
-                </Row>
-            </Container>   
+                   
+                        
+
+            
         </div>
     );
 };
--- gaza/src/pages/IDF.js
+++ gaza/src/pages/IDF.js
@@ -9,23 +9,22 @@ const IDF = () => {
                 <Card.Body>
                     <Card.Title className='card-title'>Israel Defense Forces</Card.Title>
                         <Card.Text className='card-text'>
-                        The IDF was established on May 26, 1948 following Israel's Declaration of Independence
+                        <p>The Israeli Defense Force (IDF) was established on May 26, 1948 following Israel's Declaration of Independence. Today the organization operates on a budget of pver 24.3 Billion dollars and utilizes over 500,000 personel.
+                           Israel launched their initial invasion of the Gaza Strip on the 27th of October, 2023. The stated goal of the operation is the elimination of Hamas. 
+                           In pursuit of their goals, the IDF has dropped more than 45,000 bombs throughout the region often indiscriminately harming civilian populations.
+                        </p>
+                        <p>Since Hamas siezed control of the Gaza Strip in 2007 Israel has blockaded Gaza, preventing goods from reaching the region. In response to the conflict, Israel has taken control of 
+                            access to food, water, and electricity, further burdening civilians involved in the conflict
+                        </p>
+                        <p>
+                            "We are putting a complete siege on Gaza. No electricity. No food, no water, no gas. It's all closed. We're fighting animals and are acting accordingly.” - Yoav Galant, Defense Minister of Israel, in response to the October 7th attack
+                        </p>
                         </Card.Text>
                 </Card.Body>
-            </Card>
-            <Card className='card-container'>
-                <Card.Body>
-                    <Card.Title className='card-title'></Card.Title>
-                        <Card.Text className='card-text'>
-                        </Card.Text>
-                </Card.Body>
-            </Card>
-            <Card className='card-container'>
-                <Card.Body>
-                    <Card.Title className='card-title'></Card.Title>
-                        <Card.Text className='card-text'>
-                        </Card.Text>
-                </Card.Body>
+                <div className="images-row">
+                    <img src="./yoavgalant.PNG"/>
+                    <img src="./Badge_of_the_Israeli_Defense_Forces_2022_version.svg.png" />
+                </div>
             </Card>
         </div>
     );
